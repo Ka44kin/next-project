5fa97a93da98b39a404a9ecd75a893d35a8eee7b
ka44kin <1evgeniy977@gmail.com>
2023-10-08 19:37:59 +0300

    Added footer layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,19 @@
 import { FooterProps } from './Footer.props';
 import styles from "./Footer.module.css";
-import cn from "classnames";
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
     return  (
-        <div { ...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} { ...props}>
+            <div>
+                OwlTop 2020 - {format(new Date, 'yyyy')} Все права защищены
+            </div>
+            <a href="#" target='_blank'>Пользовательское соглашение</a>
+            <a href="#" target='_blank'>Политика конфиденциальности</a>
+        </footer>
     );
 };
+{/* <p className={styles.one}>OwlTop 2020-2023 Все права защищены</p>
+            <p className={styles.two}>Пользовательское соглашение</p>
+            <p className={styles.three}>Политика конфиденциальности</p> */}
